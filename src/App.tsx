7bedba41c00117608436207b9effe3c49fed94a2
emre-cil <info.emrecil@gmail.com>
2024-07-01 01:05:16 +0300

    feat: add base ui elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [chat, setChat] = useState<{ message: string; type: "ai" | "user" }[]>(
+    []
+  );
+
+  const prompt = async () => {
+    const input = inputRef.current?.value;
+    if (!input) return;
+
+    inputRef.current.value = "";
+    setChat((prevChat) => [...prevChat, { message: input, type: "user" }]);
+
+    const newSession = await window.ai.createTextSession();
+    const response = await newSession.prompt(input);
+    setChat((prevChat) => [...prevChat, { message: response, type: "ai" }]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="app">
+      <h1>Built-in AI Chat</h1>
+
+      {chat?.length > 0 && (
+        <div className="chat-container">
+          {chat.map((item, index) => (
+            <div
+              key={index}
+              className={`chat-message ${
+                item.type === "ai" ? "chat-message--ai" : "chat-message--user"
+              }`}
+            >
+              {item.message}
+            </div>
+          ))}
+        </div>
+      )}
+      <div className="input-container">
+        <input
+          type="text"
+          className="input-field"
+          ref={inputRef}
+          placeholder="Type your message here..."
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              prompt();
+            }
+          }}
+        />
+        <button className="send-button" onClick={prompt}>
+          Send
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
